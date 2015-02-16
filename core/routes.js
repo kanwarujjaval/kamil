00c6e6029711d303bfc6c63559c3b43ccddddd55
Kanwar Ujjaval Singh <ujjavalkanwar@gmail.com>
2015-02-16 14:48:24 +0530

    fixed home

--- core/routes.js
+++ core/routes.js
@@ -4,6 +4,10 @@
 var config = require("../config");
 module.exports = function(app,passport,env){
 
+    app.get('/', function(req,res){
+        res.render('features-and-form');
+    });
+
     app.get('/views/*', function (req, res) {
         res.render(config.paths.views + "/" + req.params[0]);
     });
